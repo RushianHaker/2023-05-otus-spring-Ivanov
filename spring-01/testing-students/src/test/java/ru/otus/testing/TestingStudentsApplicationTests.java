6e2d09b44e0996762bb8d3b8140327b539756085
Max Ivanov <mamalenaralalend@gmail.com>
2023-06-05 20:26:56 +0300

    аннотация для правильной сборки

--- spring-01/testing-students/src/test/java/ru/otus/testing/TestingStudentsApplicationTests.java
+++ spring-01/testing-students/src/test/java/ru/otus/testing/TestingStudentsApplicationTests.java
@@ -1,7 +1,10 @@
 package ru.otus.testing;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 class TestingStudentsApplicationTests {
     @Test
     void contextLoads() {
