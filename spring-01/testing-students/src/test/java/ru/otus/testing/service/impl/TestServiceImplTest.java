43298b438cf3c952523fe06ccdf0a80f0ab18079
Max Ivanov <mamalenaralalend@gmail.com>
2023-06-05 20:27:43 +0300

    новый тест сервиса, проверяем работу метода getTestList()

---
+++ spring-01/testing-students/src/test/java/ru/otus/testing/service/impl/TestServiceImplTest.java
@@ -0,0 +1,28 @@
+package ru.otus.testing.service.impl;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.otus.testing.service.TestService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(SpringRunner.class)
+class TestServiceImplTest {
+
+    ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/spring-context.xml");
+    TestService service = context.getBean(TestService.class);
+
+    @Test
+    void getTestListTest() {
+        var listQuesting = service.getTestList();
+
+        assertEquals(5, listQuesting.size());
+        assertEquals(2, listQuesting.get(0).getAnswer().size());
+
+        assertEquals("That is your name?", listQuesting.get(0).getQuestion());
+        assertEquals("Max", listQuesting.get(0).getAnswer().get(0).getAnswer());
+        assertEquals("Anna", listQuesting.get(0).getAnswer().get(1).getAnswer());
+    }
+}
