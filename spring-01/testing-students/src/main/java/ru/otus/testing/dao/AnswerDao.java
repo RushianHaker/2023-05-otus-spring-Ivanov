4c6b3b6fd98da0ab3f2ad0d8f1cee4e7a898a4e6
Maxim Ivanov <m.ivanov@r77.center-inform.ru>
2023-06-06 18:26:14 +0300

    убрал лишнее DAO

--- spring-01/testing-students/src/main/java/ru/otus/testing/dao/AnswerDao.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.testing.dao;
-
-import ru.otus.testing.model.Answer;
-
-public interface AnswerDao {
-    Answer createAnswer(String name);
-}
--- spring-01/testing-students/src/main/java/ru/otus/testing/dao/impl/QuestionDaoImpl.java
+++ spring-01/testing-students/src/main/java/ru/otus/testing/dao/impl/QuestionDaoImpl.java
@@ -3,7 +3,6 @@ package ru.otus.testing.dao.impl;
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
 import org.springframework.core.io.ClassPathResource;
-import ru.otus.testing.dao.AnswerDao;
 import ru.otus.testing.dao.QuestionDao;
 import ru.otus.testing.model.Answer;
 import ru.otus.testing.model.Question;
@@ -14,12 +13,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class QuestionDaoImpl implements QuestionDao {
-
-    private final AnswerDao answerDao;
     private final ClassPathResource classPathResource;
 
-    public QuestionDaoImpl(AnswerDao answerDao, ClassPathResource classPathResource) {
-        this.answerDao = answerDao;
+    public QuestionDaoImpl(ClassPathResource classPathResource) {
         this.classPathResource = classPathResource;
     }
 
@@ -38,7 +34,7 @@ public class QuestionDaoImpl implements QuestionDao {
                     var listAnswers = new ArrayList<Answer>();
 
                     for (int i = 1; i < nextLine.length; i++) {
-                        listAnswers.add(answerDao.createAnswer(nextLine[i]));
+                        listAnswers.add(new Answer(nextLine[i]));
                     }
 
                     listQuestions.add(createQuestion(nextLine[0], listAnswers));
