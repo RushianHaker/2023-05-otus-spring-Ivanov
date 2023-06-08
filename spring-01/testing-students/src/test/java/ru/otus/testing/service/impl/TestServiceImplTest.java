b92e7b717199a48b4c375bc057d186c068d8c79c
Maxim Ivanov <m.ivanov@r77.center-inform.ru>
2023-06-08 17:43:00 +0300

    переписал интеграционный-тест на юнит-тест !

--- spring-01/testing-students/src/test/java/ru/otus/testing/service/impl/TestServiceImplTest.java
+++ spring-01/testing-students/src/test/java/ru/otus/testing/service/impl/TestServiceImplTest.java
@@ -1,30 +1,42 @@
 package ru.otus.testing.service.impl;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.otus.testing.dao.QuestionDao;
+import ru.otus.testing.model.Answer;
+import ru.otus.testing.model.Question;
 import ru.otus.testing.service.TestService;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 
 class TestServiceImplTest {
-
-    ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/spring-context.xml");
-    TestService service = context.getBean(TestService.class);
+    QuestionDao questionDao = mock(QuestionDao.class);
+    TestService service = new TestServiceImpl(questionDao);
 
     @Test
     void getTestListTest() {
+        when(questionDao.findAll()).thenReturn(
+                List.of(new Question("test question",
+                        List.of(
+                                new Answer("test answer 1", true),
+                                new Answer("test answer 2", false))
+                )));
+
         var listQuesting = service.getTest();
 
-        assertEquals(5, listQuesting.size());
+        assertEquals(1, listQuesting.size());
         assertEquals(2, listQuesting.get(0).getAnswer().size());
 
-        assertEquals("That is your name?", listQuesting.get(0).getQuestion());
+        assertEquals("test question", listQuesting.get(0).getQuestion());
 
-        assertEquals("Max", listQuesting.get(0).getAnswer().get(0).getAnswer());
+        assertEquals("test answer 1", listQuesting.get(0).getAnswer().get(0).getAnswer());
         assertTrue(listQuesting.get(0).getAnswer().get(0).isCorrect());
 
-        assertEquals("Anna", listQuesting.get(0).getAnswer().get(1).getAnswer());
+        assertEquals("test answer 2", listQuesting.get(0).getAnswer().get(1).getAnswer());
         assertFalse(listQuesting.get(0).getAnswer().get(1).isCorrect());
     }
 }
