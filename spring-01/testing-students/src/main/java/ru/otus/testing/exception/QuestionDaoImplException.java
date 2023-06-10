0cf2a3db0c7ad67481fadcc4c4e03f8520bf5704
Max Ivanov <mamalenaralalend@gmail.com>
2023-06-10 16:00:15 +0300

    создал свое исключение

---
+++ spring-01/testing-students/src/main/java/ru/otus/testing/exception/QuestionDaoImplException.java
@@ -0,0 +1,8 @@
+package ru.otus.testing.exception;
+
+
+public class QuestionDaoImplException extends RuntimeException {
+    public QuestionDaoImplException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
