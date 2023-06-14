b8ca737fd85befeff75971811fa0d8041f05573a
Max Ivanov <mamalenaralalend@gmail.com>
2023-06-14 20:30:52 +0300

    Файл настроек и файл с вопросами, для тестов д.б. свои.

--- spring-02/testing-students/src/test/java/ru/otus/testing/dao/impl/QuestionDaoImplTest.java
+++ spring-02/testing-students/src/test/java/ru/otus/testing/dao/impl/QuestionDaoImplTest.java
@@ -19,9 +19,9 @@ class QuestionDaoImplTest {
 
     @Test
     void printTest() {
-        var answer1 = new Answer("Max", true);
-        var answer2 = new Answer("Anna", false);
-        var question = new Question("That is your name?", List.of(answer1, answer2));
+        var answer1 = new Answer("Santa", true);
+        var answer2 = new Answer("Lenin", false);
+        var question = new Question("HO-HO-HO Who i am?", List.of(answer1, answer2));
 
         var questions = questionDao.findAll();
         assertNotNull(questions);
