40719a1a7d4fefed23cfe33b7dffb943812e8ca3
Max Ivanov <mamalenaralalend@gmail.com>
2023-06-15 15:45:47 +0300

    У нас для этого есть PrintService, напрямую мы System.out/in не должны использовать

--- spring-02/testing-students/src/main/java/ru/otus/testing/service/impl/TestResultServiceImpl.java
+++ spring-02/testing-students/src/main/java/ru/otus/testing/service/impl/TestResultServiceImpl.java
@@ -3,24 +3,27 @@ package ru.otus.testing.service.impl;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ru.otus.testing.model.User;
+import ru.otus.testing.service.PrintService;
 import ru.otus.testing.service.TestResultService;
 
 @Service
 public class TestResultServiceImpl implements TestResultService {
 
+    private final PrintService printService;
     private final int completeCount;
 
-    public TestResultServiceImpl(@Value("${application.completeCount}") int completeCount) {
+    public TestResultServiceImpl(PrintService printService, @Value("${application.completeCount}") int completeCount) {
+        this.printService = printService;
         this.completeCount = completeCount;
     }
 
     @Override
     public void checkTestResult(User user, int result) {
         if (result >= completeCount) {
-            System.out.println(user.getName() + " " + user.getLastName() +
+            printService.print(user.getName() + " " + user.getLastName() +
                     " you PASS the test with " + result + " points !");
         } else {
-            System.out.println(user.getName() + " " + user.getLastName() +
+            printService.print(user.getName() + " " + user.getLastName() +
                     " you NOT PASS the test, your result is " + result + " !");
         }
     }
