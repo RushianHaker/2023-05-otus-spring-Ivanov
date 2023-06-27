d79d05f2c0e032aa04ad4f42705a44e6ab446adf
Max Ivanov <mamalenaralalend@gmail.com>
2023-06-27 03:21:17 +0300

    обновил конфиг параметрами из настроек на прямую

--- spring-03/testing-students/src/main/java/ru/otus/testing/config/ApplicationConfig.java
+++ spring-03/testing-students/src/main/java/ru/otus/testing/config/ApplicationConfig.java
@@ -1,9 +1,31 @@
 package ru.otus.testing.config;
 
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Locale;
+
 @Configuration
-@EnableConfigurationProperties(AppProps.class)
 public class ApplicationConfig {
+
+    @Value("${application.locale}")
+    private Locale locale;
+
+    @Value("${application.completeCount}")
+    private int completeCount;
+
+    @Value("${application.pathToTestFile}")
+    private String pathToTestFile;
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public int getCompleteCount() {
+        return completeCount;
+    }
+
+    public String getPathToTestFile() {
+        return pathToTestFile;
+    }
 }
