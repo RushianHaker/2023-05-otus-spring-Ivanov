e9b9c0682b9ce4f6ccb4c58aba3423b81108f397
Max Ivanov <mamalenaralalend@gmail.com>
2023-06-28 01:02:27 +0300

    интеграционность заключается в работе с файловой системой

--- spring-03/testing-students/src/test/java/ru/otus/testing/dao/impl/QuestionDaoCsvImplTest.java
+++ spring-03/testing-students/src/test/java/ru/otus/testing/dao/impl/QuestionDaoCsvImplTest.java
@@ -6,23 +6,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 import ru.otus.testing.dao.QuestionDao;
 import ru.otus.testing.model.Answer;
 import ru.otus.testing.model.Question;
-import ru.otus.testing.service.MessageSourceService;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.mock;
 
 @SpringBootTest
-class QuestionDaoImplTest {
+class QuestionDaoCsvImplTest {
 
     private QuestionDao questionDao;
 
     @BeforeEach
     public void init() {
-        MessageSourceService messageSourceService = mock(MessageSourceService.class);
-        questionDao = new QuestionDaoImpl(messageSourceService, "test.csv");
+        questionDao = new QuestionDaoCsvImpl("test_en.csv");
     }
 
     @Test
