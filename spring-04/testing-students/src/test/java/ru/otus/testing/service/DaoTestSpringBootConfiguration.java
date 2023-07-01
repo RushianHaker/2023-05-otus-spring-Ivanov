5d3f1039f8b2790d015bb072c449b1a4f5e139aa
Max Ivanov <mamalenaralalend@gmail.com>
2023-07-01 07:51:43 +0300

    тестовая конфигурация

---
+++ spring-04/testing-students/src/test/java/ru/otus/testing/service/DaoTestSpringBootConfiguration.java
@@ -0,0 +1,10 @@
+package ru.otus.testing.service;
+
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(value = "ru.otus.testing.service")
+@SpringBootConfiguration
+public class DaoTestSpringBootConfiguration {
+}
