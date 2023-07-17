3c42e09c658b94f29fe1cfecda58f0b1501bf9c8
Max Ivanov <mamalenaralalend@gmail.com>
2023-07-17 14:57:39 +0300

    нейминг

--- spring-05/testing-students/src/main/java/ru/otus/testing/LibraryApplication.java
+++ spring-05/testing-students/src/main/java/ru/otus/testing/LibraryApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestingStudentsApplication {
+public class LibraryApplication {
     public static void main(String[] args) {
-        SpringApplication.run(TestingStudentsApplication.class, args);
+        SpringApplication.run(LibraryApplication.class, args);
     }
 }
