a20b7d09811ed59059709f08f7ba07b927184ff7
Max Ivanov <mamalenaralalend@gmail.com>
2023-08-17 17:17:17 +0300

    Причем тут Jdbc и как это используется?

--- spring-06/library/src/main/java/ru/otus/testing/exception/AuthorDaoJdbcException.java
+++
@@ -1,8 +0,0 @@
-package ru.otus.testing.exception;
-
-
-public class AuthorDaoJdbcException extends RuntimeException {
-    public AuthorDaoJdbcException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
--- spring-06/library/src/main/java/ru/otus/testing/exception/GenreDaoJdbcException.java
+++
@@ -1,8 +0,0 @@
-package ru.otus.testing.exception;
-
-
-public class GenreDaoJdbcException extends RuntimeException {
-    public GenreDaoJdbcException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
