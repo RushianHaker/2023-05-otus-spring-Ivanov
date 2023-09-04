9deb7834078da978f473514009c71470932b820b
Max Ivanov <mamalenaralalend@gmail.com>
2023-09-05 01:47:27 +0300

    Достаточно просто сохранить комментарий с книгой внутри

--- spring-06/library/src/main/java/ru/otus/testing/service/impl/CommentServiceImpl.java
+++ spring-06/library/src/main/java/ru/otus/testing/service/impl/CommentServiceImpl.java
@@ -1,9 +1,7 @@
 package ru.otus.testing.service.impl;
 
 import org.springframework.stereotype.Service;
-import ru.otus.testing.dao.BookDao;
 import ru.otus.testing.dao.CommentDao;
-import ru.otus.testing.exceptions.BookServiceException;
 import ru.otus.testing.model.Comment;
 import ru.otus.testing.service.CommentService;
 
@@ -12,30 +10,20 @@ import java.util.ArrayList;
 @Service
 public class CommentServiceImpl implements CommentService {
 
-    private final BookDao bookDao;
-
     private final CommentDao commentDao;
 
-    public CommentServiceImpl(BookDao bookDao, CommentDao commentDao) {
-        this.bookDao = bookDao;
+    public CommentServiceImpl(CommentDao commentDao) {
         this.commentDao = commentDao;
     }
 
     @Override
     public void saveBooksComment(Comment comment) {
         var book = comment.getBook();
-
-        if (book == null) {
-            throw new BookServiceException("Book not found!");
-        }
-
         if (book.getComments() == null) {
             book.setComments(new ArrayList<>());
+            comment.setBook(book);
         }
 
-        var commentInfoFromDb = commentDao.save(comment);
-        book.getComments().add(commentInfoFromDb);
-
-        bookDao.save(book);
+        commentDao.save(comment);
     }
 }
